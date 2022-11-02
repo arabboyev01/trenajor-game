30df7a09d59186a95e6076cb3ab0b9397e9f5867
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2022-11-02 11:15:09 +0500

    fix: Start page

--- components/StartPage/StartPage.tsx
+++ components/StartPage/StartPage.tsx
@@ -16,27 +16,8 @@ import startPageBg from "../assets/startpage/start-bg.png";
 import {useEffect, useState} from "react";
 export default function StartPage(){
 
-    const [InitValue, setInitValue] = useState("")
-    const [SecValue, setSecValue] = useState("");
-    const [linked, setLinked] = useState("/");
-    const [alert, setAlert] = useState(undefined);
-
-    const alertMessage: any = <Stack style={{width: "300px", position: "absolute", right: "10px", top: "30px"}}><Alert severity="info"><AlertTitle>Empty Value</AlertTitle>Please — <strong>Choose a value</strong></Alert></Stack>
-    useEffect(() => {
-    }, [InitValue, SecValue, alert, linked])
-
-    const getValue = function(){
-        if(InitValue && SecValue){
-            setLinked("/main");
-            setAlert(undefined);
-        }else{
-            setLinked("/");
-            setAlert(alertMessage);
-        }
-    }
     return(
         <StartPageWrapper style={{background: `url(${startPageBg.src})`}}>
-            {alert}
             <StartModal>
                 <QuantityItem>Кол-во предметов</QuantityItem>
                 <RangeNumsRow>
@@ -45,7 +26,7 @@ export default function StartPage(){
                     <RangeNums>4</RangeNums>
                     <RangeNums>5</RangeNums>
                 </RangeNumsRow>
-                <InputRange type={"range"} onChange={(e) => setInitValue(e.target.value)} defineWidth={"366px"} min="0" max="5" value="0" step="1"/>
+                <InputRange type={"range"}  defineWidth={"366px"} min="0" max="5" value="0" step="1"/>
                 <QuantityItem>Значения</QuantityItem>
                 <RangeNumsRow>
                     <RangeNums>A</RangeNums>
@@ -55,13 +36,13 @@ export default function StartPage(){
                     <RangeNums>99</RangeNums>
                     <RangeNums>999</RangeNums>
                 </RangeNumsRow>
-                <InputRange type={"range"} defineWidth={"531px"} min="0" max="100" value="0" step="10" onChange={e => setSecValue(e.target.value)}/>
+                <InputRange type={"range"} defineWidth={"531px"} min="0" max="100" value="0" step="10" />
                 <ButtonWrapper>
                     <Button defaultOpacity={"1"}>По возрастанию</Button>
                     <Button defaultOpacity={"0.7"}>По убыванию</Button>
                 </ButtonWrapper>
-                <Link href={linked}>
-                    <StartButton onClick={getValue}>Играть</StartButton>
+                <Link href="/main" style={{textDecoration: "none"}}>
+                    <StartButton >Играть</StartButton>
                 </Link>
             </StartModal>
         </StartPageWrapper>
