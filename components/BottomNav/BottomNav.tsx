d2ccf076d56aefb1de382a349937d07ef6b0bdac
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2022-11-02 10:10:38 +0500

    fix: Bottom nav

--- components/BottomNav/BottomNav.tsx
+++ components/BottomNav/BottomNav.tsx
@@ -1,4 +1,4 @@
-import {BottomNavWrapper, SingleCoin} from "./bottomnav.style";
+import {BottomNavWrapper, CoinPlaces, SingleCoin} from "./bottomnav.style";
 import BottomImage from "../assets/mainPage/bottom1.png";
 import Draggable from 'react-draggable';
 import { coinsDumbData } from "./coinsDumbData";
@@ -8,11 +8,13 @@ export default function BottomNav(){
         <BottomNavWrapper style={{backgroundImage: `url(${BottomImage.src})`}}>
 
                 {coinsDumbData.map(item =>
+                    <CoinPlaces>
                     <Draggable>
                         <div>
                     <SingleCoin key={item.id} style={{backgroundImage: `url(${item.image.src})`}}></SingleCoin>
                         </div>
                     </Draggable>
+                    </CoinPlaces>
                 )}
         </BottomNavWrapper>
     )
--- components/BottomNav/bottomnav.style.ts
+++ components/BottomNav/bottomnav.style.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 export const BottomNavWrapper = styled.section`
-       width: 886px;
+       width: 900px;
        height: 222px;
        background-position: center center;
        background-size: cover;
@@ -8,11 +8,19 @@ export const BottomNavWrapper = styled.section`
        flex-direction: row;
        align-items: center;
        justify-content: space-between;
+       padding: 0 20px;
 `
 export const SingleCoin = styled.div`
        background-size: contain;
        background-position: center;
-       width: 157px;
-       height: 157px;
+       width: 145px;
+       height: 145px;
        border-radius: 50%;
+`
+export const CoinPlaces = styled.div`
+       width: 145px;
+       height: 145px;
+       border-radius: 50%;
+       background: rgba(0, 0, 0, 0.06);
+       box-shadow: inset 0px 4px 25px rgba(0, 0, 0, 0.25);
 `
